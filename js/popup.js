10a0a3a95b60a536cfed2aee4784fa85d604ee70
Evan Jarrett <ejfirestar00@gmail.com>
2018-08-29 22:36:57 -0700

    more work

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,32 @@
-let header = { 'name': "x-op-test", 'value': "this is a test"};
 
-// Put the object into storage
-localStorage.setItem('header', JSON.stringify(header));
+let headers = document.getElementsByClassName("header");
+
+function saveHeader(header) {
+    chrome.storage.sync.set({
+        header: header
+    }, function() {
+        console.log("Saved");
+    });
+}
+
+function restore_options() {
+    // Use defaults
+    chrome.storage.sync.get({
+        header: { "name": "", "value": ""},
+    }, function(items) {
+        for (let i = 0; i < headers.length; i++) {
+            headers[i].getElementsByClassName("name")[0].value = items.header.name;
+            headers[i].getElementsByClassName("value")[0].value = items.header.value;
+        }
+    });
+}
+
+for (let i = 0; i < headers.length; i++) {
+    let name = headers[i].getElementsByClassName("name")[0];
+    let value = headers[i].getElementsByClassName("value")[0];
+    name.addEventListener('input', saveHeader);
+    value.addEventListener('input', saveHeader);
+    saveHeader({ "name": name.value, "value": value.value});
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
