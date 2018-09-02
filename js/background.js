c65e11d39a45b0c93198da1b991afbe77262e3bd
Evan Jarrett <ejfirestar00@gmail.com>
2018-09-02 01:05:28 -0700

    ability to add more headers and proper syncing in the background

--- js/background.js
+++ js/background.js
@@ -8,15 +8,28 @@ function modifyHeaders(details) {
     console.log(details.requestHeaders);
     return {requestHeaders: details.requestHeaders};
 }
+
 chrome.webRequest.onBeforeSendHeaders.addListener(
     modifyHeaders,
     {urls: ["<all_urls>"]},
     ['requestHeaders', 'blocking']
 );
-chrome.storage.sync.get({
-    "headers": [],
-}, function(items) {
-    localStorage.setItem("headers", JSON.stringify(items.headers));
-    console.log("Restore2");
-    console.log(items);
-});
+
+function syncHeaders() {
+    chrome.storage.sync.get({
+        "headers": [],
+    }, function (items) {
+        localStorage.setItem("headers", JSON.stringify(items.headers));
+        console.log("Restore2");
+        console.log(items);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', syncHeaders);
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        syncHeaders();
+        console.log("syncing headers from main")
+    }
+);
--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,5 @@
-
 let doc_headers = document.getElementsByClassName("header");
+let row_count = 0;
 
 function saveHeaders() {
     let headers = [];
@@ -9,22 +9,36 @@ function saveHeaders() {
         if (name.value === "" || value.value === "") {
             continue;
         }
-        headers.push({ "name": name.value, "value": value.value})
+        headers.push({"name": name.value, "value": value.value})
     }
 
     chrome.storage.sync.set({
-    "headers": headers
-    }, function() {
+        "headers": headers
+    }, function () {
         console.log("Saved");
         console.log(headers);
     });
+    chrome.runtime.sendMessage({headers: "headers"});
+}
+
+function addListeners() {
+    for (let i = 0; i < doc_headers.length; i++) {
+        let name = doc_headers[i].getElementsByClassName("name")[0];
+        let value = doc_headers[i].getElementsByClassName("value")[0];
+        name.addEventListener('input', saveHeaders);
+        value.addEventListener('input', saveHeaders);
+    }
 }
 
 function restore_options() {
     // Use defaults
     chrome.storage.sync.get({
-        "headers": { "name": "", "value": ""},
-    }, function(items) {
+        "headers": {"name": "", "value": ""},
+        "count": row_count
+    }, function (items) {
+        for (let i = 0; i < items.count; i++) {
+            addRow();
+        }
         for (let i = 0; i < doc_headers.length; i++) {
             if (items.headers[i].value === "" || items.headers[i].value === "") {
                 continue;
@@ -34,14 +48,29 @@ function restore_options() {
         }
         console.log("Restore");
         console.log(items);
+        addListeners();
     });
 }
 
-for (let i = 0; i < doc_headers.length; i++) {
-    let name = doc_headers[i].getElementsByClassName("name")[0];
-    let value = doc_headers[i].getElementsByClassName("value")[0];
+function addRow() {
+    let row = document.querySelector("#first_row");
+    let clone = row.cloneNode(true);
+    clone.removeAttribute("id");
+    document.querySelector("#header_selector").appendChild(clone);
+    let name = clone.getElementsByClassName("name")[0];
+    let value = clone.getElementsByClassName("value")[0];
     name.addEventListener('input', saveHeaders);
     value.addEventListener('input', saveHeaders);
+    row_count++;
+    chrome.storage.sync.set({
+        "count": row_count
+    }, function () {
+        console.log("Saved count");
+        console.log(row_count);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
+
+addButton = document.querySelector("#add_button");
+addButton.addEventListener('click', addRow);
