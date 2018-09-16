4e68aa0af11dcf8074e9ec3702286edc2d951268
Evan Jarrett <ejfirestar00@gmail.com>
2018-09-15 19:20:03 -0700

    more updates

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,8 @@
 let doc_headers = document.getElementsByClassName("header");
-let row_count = 0;
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector(".active").addEventListener("input", saveHeaders);
-document.querySelector(".clear").addEventListener("click", removeRow);
 
-addButton = document.querySelector("#add_button");
+const addButton = document.querySelector("#add_button");
 addButton.addEventListener("click", addRow);
 
 const ibutton = document.querySelector('#import_button');
@@ -47,46 +44,34 @@ function saveHeaders() {
     chrome.runtime.sendMessage({headers: "headers"});
 }
 
-function addListeners() {
-    for (let i = 0; i < doc_headers.length; i++) {
-        let name = doc_headers[i].querySelector(".name");
-        let value = doc_headers[i].querySelector(".value");
-        let active = doc_headers[i].querySelector(".active");
-        name.addEventListener("input", saveHeaders);
-        value.addEventListener("input", saveHeaders);
-        active.addEventListener("change", saveHeaders);
-    }
-}
-
 function restoreOptions() {
     // Use defaults
     chrome.storage.sync.get({
         "headers": {"name": "", "value": "", "active": ""},
-        "count": row_count
     }, function (items) {
-        for (let i = 0; i < items.count; i++) {
+        for (let i = 0; i < items.headers.length; i++) {
             addRow();
             if (items.headers[i].name === "" || items.headers[i].value === "") {
                 continue;
             }
+
             doc_headers[i].querySelector(".name").value = items.headers[i].name;
             doc_headers[i].querySelector(".value").value = items.headers[i].value;
-            doc_headers[i].querySelector(".active").checked = items.headers[i].active;
+            const a = doc_headers[i].querySelector(".active");
+            a.checked = items.headers[i].active;
+            if (a.checked) {
+                const l = a.closest(".label");
+                l.classList.add("is-checked");
+                componentHandler.upgradeElements(a);
+                componentHandler.upgradeElements(l);
+            }
         }
         console.log("Restore");
         console.log(items);
-        addListeners();
     });
 }
 
 function removeRow() {
-    row_count--;
-    chrome.storage.sync.set({
-        "count": row_count
-    }, function () {
-        console.log("Removed Row");
-        console.log(row_count);
-    });
     this.closest(".row").remove();
     saveHeaders();
 }
@@ -117,12 +102,20 @@ function addRow() {
     value.addEventListener("input", saveHeaders);
     active.addEventListener("input", saveHeaders);
     clear.addEventListener("click", removeRow);
-
-    row_count++;
-    chrome.storage.sync.set({
-        "count": row_count
-    }, function () {
-        console.log("Added Row");
-        console.log(row_count);
-    });
 }
+// <span class="mdl-list__item-secondary-action">
+//     <label id="labelud0t57" class="label mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect mdl-js-ripple-effect--ignore-events is-upgraded is-checked" for="ud0t57" data-upgraded=",MaterialCheckbox,MaterialRipple">
+//     <input type="checkbox" id="ud0t57" class="active mdl-checkbox__input">
+//     <span class="mdl-checkbox__focus-helper"></span><span class="mdl-checkbox__box-outline"><span class="mdl-checkbox__tick-outline"></span>
+// </span><span class="mdl-checkbox__ripple-container mdl-js-ripple-effect mdl-ripple--center" data-upgraded=",MaterialRipple">
+// <span class="mdl-ripple">
+// </span></span></label>
+// </span>
+// <span class="mdl-list__item-secondary-action">
+//     <label id="labelvrkqr" class="label mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect is-checked mdl-js-ripple-effect--ignore-events is-upgraded" for="vrkqr" data-upgraded=",MaterialCheckbox,MaterialRipple">
+//     <input type="checkbox" id="vrkqr" class="active mdl-checkbox__input">
+//     <span class="mdl-checkbox__focus-helper"></span><span class="mdl-checkbox__box-outline"><span class="mdl-checkbox__tick-outline"></span>
+// </span><span class="mdl-checkbox__ripple-container mdl-js-ripple-effect mdl-ripple--center" data-upgraded=",MaterialRipple">
+// <span class="mdl-ripple is-animating" style="width: 103.823px; height: 103.823px; transform: translate(-50%, -50%) translate(18px, 18px);">
+// </span></span></label>
+// </span>
